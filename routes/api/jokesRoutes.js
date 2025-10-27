f7fb172f9089b602c94f60ebbb85c709c355f289
jpa1994 <john.astvazaturov@mscoding.org>
2025-10-27 14:04:16 -0500

    changed 404 error page

--- routes/api/jokesRoutes.js
+++ routes/api/jokesRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const PORT = process.env.PORT || 3001
 
 // http://localhost:3001/jokes
 router.get('/', (req, res)=> {
-    res.send('This works.')
+    // res.send('This works.')
+    const url = `https://api.sampleapis.com/jokes/goodJokes`
+
+    axios.get(url)
+        .then(resp => console.log(resp))
+        .then(res.send('data received'))
+
 })
 
 module.exports = router
--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
-//http://localhost:3001
+//http://localhost:3001 => home page
 router.get('/', (req, res)=> {
-    res.send('working...')
+    // res.send('working...') => display home page
+    res.render('pages/home',  {
+        title: "John's Jokes App",
+        name: "john's jokes app"
+    })
 })
 
 router.use('/jokes', require('./api/jokesRoutes'))
 
 // error handling
 router.use((req, res, next) => {
-    res.status(404).send('404 Error. This page does not exist.')
+    res.status(404).render('pages/404', {
+        title: '404 Error',
+        name: '404 Error. This page does not exist.'
+    })
 })
 
 module.exports = router
