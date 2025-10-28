47cf3aa6ce609824c76286d3c0b96f3daadd3a65
jpa1994 <john.astvazaturov@mscoding.org>
2025-10-28 15:33:22 -0500

    built pagination helpers

--- routes/api/jokesRoutes.js
+++ routes/api/jokesRoutes.js
@@ -1,22 +1,71 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const { paginationResults, buildJokeArr } = require('../../helpers/pagination')
 const PORT = process.env.PORT || 3001
 
 // http://localhost:3001/jokes
 router.get('/', (req, res)=> {
     // res.send('This works.')
     const url = `https://api.sampleapis.com/jokes/goodJokes`
+    /** pagination... */
+    const pageData = paginationResults(req)
+   
+    // will store jokes in here
+    let jokesArr = []
 
     axios.get(url)
         .then(resp => {
+            
+            // for (let i = pageData.startIdx; i < pageData.endIdx; i++) {
+            //     if (resp.data[i] != undefined) {
+            //         jokesArr = [...jokesArr, resp.data[i]]
+            //     }
+            // }
+
+            // const prev = pageData.page > 1 ? pageData.page - 1 :null
+            // const next = pageData.endIdx >= resp.data.length ? null : pageData.page + 1
+            const jokeArrData = buildJokeArr(resp.data, jokesArr, pageData.startIdx, pageData.endIdx, pageData.page)
+
+            // console.log(jokesArr)
+        
             res.render('pages/allJokes', {
                 title: 'All Jokes',
                 name: 'All Jokes',
-                data: resp.data
+                data: jokeArrData.arr,
+                prev: jokeArrData.prev,
+                next: jokeArrData.next
             })
         })
 
+})
+
+// Joke Type
+// localhost:3001/jokes
+router.get('/type/:type', (req, res) => {
+
+    const type = req.params.type
+    const url = `https://api.sampleapis.com/jokes/goodJokes`
+    const pageData = paginationResults(req)
+
+    // We will filter through resp.data and store in typeArr
+    let typeArr = []
+
+    let jokesArr = []
+
+    axios.get(url)
+        .then(resp => typeArr = resp.data.filter(item => item.type == type))
+        .then(typeArr => {
+
+            buildJokeArr(typeArr, jokesArr, pageData.startIdx, pageData.endIdx, pageData.page)
+            res.render('pages/allJokes', {
+                title: type,
+                name: `${type} jokes`,
+                data: jokeArrData.arr,
+                prev: jokeArrData.prev,
+                next: jokeArrData.next
+            })
+        })
 })
 
 module.exports = router
